71d0a4bcb773fc7d5a73cb754d9b2f1fbe21277b
shanyuehua7 <2562017477@qq.com>
2026-08-16 03:53:35 +0800

    refactor: 项目重命名为 train-ticketing，模块前缀/包名统一并升级 JDK21 依赖

---
+++ train-ticketing-member/src/main/java/com/trainticketing/member/controller/MemberController.java
@@ -0,0 +1,53 @@
+package com.trainticketing.member.controller;
+
+import com.trainticketing.common.resp.CommonResp;
+import com.trainticketing.member.resp.MemberLoginResp;
+import com.trainticketing.member.service.MemberService;
+import com.trainticketing.req.MemberLoginReq;
+import com.trainticketing.req.MemberRegisterReq;
+import com.trainticketing.req.MemberSendCodeReq;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+  @Resource
+  private MemberService memberService;
+
+  @GetMapping("/count")
+  public CommonResp<Integer> count() {
+    int count = memberService.count();
+    CommonResp<Integer> commonResp = new CommonResp<>();
+    commonResp.setContent(count);
+    return commonResp;
+  }
+
+  @PostMapping("/register")
+  public CommonResp<Long> register(@Valid MemberRegisterReq req) {
+    long register = memberService.register(req);
+   /* CommonResp<Long> commonResp = new CommonResp<>();
+    commonResp.setContent(register);*/
+    /*  return commonResp;*/
+    return new CommonResp<>(register);
+  }
+
+
+  @PostMapping("/send-code")
+  public CommonResp<Long> sendCode(@Valid MemberSendCodeReq req){
+    memberService.sendCode(req);
+    return new CommonResp<>();
+  }
+
+  @PostMapping("/login")
+  public CommonResp<MemberLoginResp> login (@Valid MemberLoginReq req){
+    MemberLoginResp resp = memberService.login(req);
+    return new CommonResp<>(resp);
+  }
+
+}
